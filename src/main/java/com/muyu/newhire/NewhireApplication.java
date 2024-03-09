ec914dabb130169251a6498919ee8977379df1dc
zhouyu <muyu.zhouyu@outlook.com>
2024-03-09 20:47:02 +0800

    init

---
+++ src/main/java/com/muyu/newhire/NewhireApplication.java
@@ -0,0 +1,13 @@
+package com.muyu.newhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewhireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewhireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muyu/newhire/NewhireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muyu.newhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewhireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
